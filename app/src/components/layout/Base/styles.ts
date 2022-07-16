cb04492f75b27258facb517c91109ea25ad2b502
Your Name <kronicx-band@hotmail.com>
2022-07-17 05:49:43 +1000

    Update styles.ts

--- app/src/components/layout/Base/styles.ts
+++ app/src/components/layout/Base/styles.ts
@@ -1,11 +1,12 @@
 import { Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+const RaffleBG.png = 'url(RaffleBG.png)';
 export const useStyles = makeStyles((theme: Theme) => ({
   background: {
     width: '100vw',
     height: '100vh',
-    backgroundImage: 'src(RaffleBG.png)',
+    backgroundImage: RaffleBG.png,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
